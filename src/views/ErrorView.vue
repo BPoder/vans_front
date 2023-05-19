a0e0cc2fa65f6c6e0ce22b89a03e5fbfa095a71c
BPoder <bertpoder@gmail.com>
2023-05-19 16:51:30 +0300

    PADRE-BOLEANIS WIP

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Siin pole midagi ehk see ei vääri küünlaid</h1>
+    <font-awesome-icon :icon="['fas', 'menorah']" size="2xl"/>
+</template>
+
+<script>
+export default {
+    name: "ErrorView"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,36 @@
 <template>
-
-</template>
-
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <form>
+                    <div class="container text-center, mb-3, col-3">
+                        <label for="exampleInputEmail1" class="form-label">Kasutajanimi</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="container text-center, mb-3, col-3">
+                        <label for="exampleInputPassword1" class="form-label">Salasõna</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                    </div>
+                    <button type="Submit" class="btn btn-primary">Logi sisse</button>
+                </form>
+            </div>
+        </div>
+    </div></template>
 <script>
 export default {
     name: "LoginView"
 }
 </script>
-
 <style scoped>
+</style> (edited)
 
-</style>
+
+
+
+
+
+
+
+
